26662a523d76ce51a97e33f349c45b9ca4393b95
Muharrem <muharrrem@clouditsoft.com>
2019-05-03 14:06:03 +0300

    kullanıcı paneli güncelleme

--- resources/views/admin/user/show.blade.php
+++ resources/views/admin/user/show.blade.php
@@ -126,7 +126,7 @@
                                                                         <span class="mt-action-dot bg-red"></span>
                                                                         <span class="mt=action-time">9:30-13:00</span>
                                                                     </div>
-                                                                    <div class="mt-action-buttons" style="width: 175px">
+                                                                    <div class="mt-action-buttons" style="width: 22%">
                                                                         <div class="btn-group btn-group-circle">
                                                                             <button type="button" class="btn btn-outline green btn-sm">Katıldı</button>
                                                                             <button type="button" class="btn btn-outline red btn-sm active">Katılmadı</button>
