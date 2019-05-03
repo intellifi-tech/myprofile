88a372da37e606fb0b422d91c1417bc299c11d19
Muharrem <muharrrem@clouditsoft.com>
2019-05-03 14:24:19 +0300

    güncelleme

--- resources/lang/en/auth.php
+++ resources/lang/en/auth.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'failed' => 'These credentials do not match our records.',
+    'failed' => 'Bilgileri kontrol ederek tekrar deneyiniz.',
     'throttle' => 'Too many login attempts. Please try again in :seconds seconds.',
 
 ];
