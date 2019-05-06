bf3ec21cd84fdd034d61609f49b4357b4a2b7865
Muharrem <muharrrem@clouditsoft.com>
2019-05-06 16:12:17 +0300

    güncelleme

--- routes/api.php
+++ routes/api.php
@@ -15,8 +15,8 @@ use Illuminate\Http\Request;
 
 // region Kullanıcılar
 Route::get('user/index', 'Api\UserController@index');
-Route::post('user/show/{api_token}', 'Api\UserController@show');
-Route::post('user/update/{api_token}', 'Api\UserController@update');
+Route::post('user/show', 'Api\UserController@show');
+Route::post('user/update', 'Api\UserController@update');
 Route::post('user/login', 'Api\UserController@login');
 Route::post('user/register', 'Api\UserController@register');
 // endregion
