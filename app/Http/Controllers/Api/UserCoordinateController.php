433b3333df16a7a2658d127f0aa661b15104f24d
Muharrem <muharrem@clouditsoft.com>
2019-05-16 15:11:24 +0300

    Api Güncelleme

--- app/Http/Controllers/Api/UserCoordinateController.php
+++ app/Http/Controllers/Api/UserCoordinateController.php
@@ -41,4 +41,40 @@ class UserCoordinateController extends Controller
             return response()->json($json, 200, [], JSON_UNESCAPED_UNICODE);
         }
     }
+
+    public function nearbyUsers(Request $request)
+    {
+        if ($request->api_token) {
+            $user = User::where('api_token', $request->api_token)->first();
+            if ($user) {
+                if ($request->latitude && $request->longitude) {
+
+                    $coordinates = UserCoordinate::all();
+                    return response()->json($coordinates, 200, [], JSON_UNESCAPED_UNICODE);
+//                    for ($i = 0; $coordinates->count() > $i; $i++){
+//                        distance($request->latitude, $request->longitude, $coordinates->latitude, $coordinates->longitude, "M");
+//                        $user = UserCoordinate::where('latitude', $request->latitude)->where('longitude', $request->longitude)->first();
+//                    }
+
+
+//                    $json['status'] = 1;
+//                    $json['message'] = "Success";
+//                    $json['api_token'] = $user->api_token;
+//                    return response()->json($json, 200, [], JSON_UNESCAPED_UNICODE);
+                } else {
+                    $json['status'] = 0;
+                    $json['message'] = "Enlem ve boylam boş olamaz.";
+                    return response()->json($json, 200, [], JSON_UNESCAPED_UNICODE);
+                }
+            } else {
+                $json['status'] = 0;
+                $json['message'] = "Api_token geçersizdir.";
+                return response()->json($json, 200, [], JSON_UNESCAPED_UNICODE);
+            }
+        } else {
+            $json['status'] = 0;
+            $json['message'] = "Api token boş olamaz";
+            return response()->json($json, 200, [], JSON_UNESCAPED_UNICODE);
+        }
+    }
 }
