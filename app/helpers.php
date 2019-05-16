b5e82574f16886dfa62669aad0d6740b836cc83b
Muharrem <muharrem@clouditsoft.com>
2019-05-16 15:23:00 +0300

    Update helpers.php

--- app/helpers.php
+++ app/helpers.php
@@ -290,10 +290,10 @@ function getNameSurname()
 
 function distance($lat1, $lon1, $lat2, $lon2, $unit)
 {
+    dd("burada");
     if (($lat1 == $lat2) && ($lon1 == $lon2)) {
         return 0;
     } else {
-        dd("burada");
         $theta = $lon1 - $lon2;
         $dist = sin(deg2rad($lat1)) * sin(deg2rad($lat2)) + cos(deg2rad($lat1)) * cos(deg2rad($lat2)) * cos(deg2rad($theta));
         $dist = acos($dist);
