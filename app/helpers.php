37ada1c8a4c30a85f917bfa82ee3da5a23c5d01a
Muharrem <muharrem@clouditsoft.com>
2019-05-16 15:22:24 +0300

    Api Güncelleme

--- app/helpers.php
+++ app/helpers.php
@@ -293,6 +293,7 @@ function distance($lat1, $lon1, $lat2, $lon2, $unit)
     if (($lat1 == $lat2) && ($lon1 == $lon2)) {
         return 0;
     } else {
+        dd("burada");
         $theta = $lon1 - $lon2;
         $dist = sin(deg2rad($lat1)) * sin(deg2rad($lat2)) + cos(deg2rad($lat1)) * cos(deg2rad($lat2)) * cos(deg2rad($theta));
         $dist = acos($dist);
@@ -305,7 +306,6 @@ function distance($lat1, $lon1, $lat2, $lon2, $unit)
         } else if ($unit == "N") {
             return ($miles * 0.8684);
         } else if ($unit == "M") {
-            dd("burada");
             if (($miles * 1.609344) * 1000 < 100)
             {
                 return $lat2;
