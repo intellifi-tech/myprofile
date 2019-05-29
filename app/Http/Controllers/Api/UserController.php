7a84ccf39b21f43dafb9d7fc63fe84361dc3301a
Muharrem <muharrem@clouditsoft.com>
2019-05-29 16:36:00 +0300

    Api

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -106,7 +106,7 @@ class UserController extends Controller
 
     public function login(Request $request)
     {
-        dd($request->input());
+        dd($request->json()->all());
         $user = User::where('email', $request->email)->first();
         if($user->type == 1){
             if ($request->email && $request->password){
