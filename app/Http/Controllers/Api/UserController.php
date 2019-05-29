79d1898473d71f3212dbf7ea5ba502d583230bbf
Muharrem <muharrem@clouditsoft.com>
2019-05-29 15:51:39 +0300

    Update UserController.php

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -106,7 +106,7 @@ class UserController extends Controller
 
     public function login(Request $request)
     {
-        dd($request->input('email'));
+        dd($request->input('password'));
         $user = User::where('email', $request->email)->first();
         if($user->type == 1){
             if ($request->email && $request->password){
