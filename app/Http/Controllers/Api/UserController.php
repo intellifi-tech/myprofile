afc50f11d04d3ce80740266a26c2db6632d110f3
muharrem <muharrem@clouditsoft.com>
2019-06-11 14:51:02 +0300

    etkinliğe katıldım api

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -207,7 +207,7 @@ class UserController extends Controller
         if ($request->header('api-token')) {
             $user = User::where('api_token', $request->header('api-token'))->first();
             if ($user) {
-                $activities = UserAttendedActivities::where('user_id', $user->id)->get();
+                $activities = UserAttendedActivities::where('user_id', $user->id)->with(['event'])->get();
 
                 $json['status'] = 1;
                 $json['message'] = "Success";
