bc18b1377a9d51167d4460ed1632444458da79ab
muharrem <muharrem@clouditsoft.com>
2019-06-11 20:15:16 +0300

    etkinliğe katıldım api

--- app/Http/Controllers/Api/UserCoordinateController.php
+++ app/Http/Controllers/Api/UserCoordinateController.php
@@ -56,8 +56,6 @@ class UserCoordinateController extends Controller
                         array_push($nearbyUserCoordinates, $distance);
                     }
                     $nearbyUserCoordinates = array_filter($nearbyUserCoordinates);
-
-                    dd($nearbyUserCoordinates);
                     $json['status'] = 1;
                     $json['message'] = "Success";
                     $json['nearbyUserCoordinates'] = $nearbyUserCoordinates;
