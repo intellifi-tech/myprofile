f75fc0a9e4f593c6458d73ff7e8357db172395fd
muharrem <muharrem@clouditsoft.com>
2019-06-11 20:27:32 +0300

    etkinliğe katıldım api

--- app/helpers.php
+++ app/helpers.php
@@ -312,7 +312,7 @@ function distance($lat1, $lon1, $lat2, $lon2, $unit, $meterLimit, $user)
                 $coords["lon"] = $lon2;
                 return $coords;
             }else{
-                $coords["coord"] = "";
+                $coords["user"] = "";
                 return $coords;
             }
         } else {
