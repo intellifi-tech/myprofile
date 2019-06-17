04de78044c3fe3b0b269674587fdb7b7ecd4104a
Muharrem <muharrem@clouditsoft.com>
2019-06-17 13:48:43 +0300

    güncelleme

--- app/Http/Controllers/Api/EventController.php
+++ app/Http/Controllers/Api/EventController.php
@@ -202,7 +202,7 @@ class EventController extends Controller
                     }
 
                     foreach ($nearbyEvents as $event){
-                        dd($event);
+                        dd($event["event"]);
                     }
 
                 } else {
