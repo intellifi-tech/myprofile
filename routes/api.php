a0d0ea39196db80422590516c3ddadb42797f4d6
Muharrem <muharrem@clouditsoft.com>
2019-06-17 11:55:09 +0300

    event alt yapısı

--- routes/api.php
+++ routes/api.php
@@ -30,6 +30,7 @@ Route::post('user/addPhoto', 'Api\UserPhotoGalleryController@addPhoto');
 // endregion
 
 // region Etkinlikler
+Route::post('event/search', 'Api\EventController@searchEvent');
 Route::post('event/index', 'Api\EventController@index');
 Route::post('event/create', 'Api\EventController@create');
 Route::post('event/show', 'Api\EventController@show');
