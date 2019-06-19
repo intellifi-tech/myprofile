1de6befedd744d7cd12208aa47aadd0a96c98e64
muharrem <muharrem@clouditsoft.com>
2019-06-19 12:20:14 +0300

    mesaj izni

--- app/Http/Controllers/Api/UserFollowController.php
+++ app/Http/Controllers/Api/UserFollowController.php
@@ -39,14 +39,22 @@ class UserFollowController extends Controller
             $user = User::where('api_token', $request->header('api-token'))->first();
             if ($user) {
                 if ($request->to_user_id) {
-                    $follow = new Follow();
-                    $follow->from_user_id = $user->id;
-                    $follow->to_user_id = $request->to_user_id;
-                    $follow->save();
 
-                    $json['status'] = 200;
-                    $json['message'] = "Success";
-                    return response()->json($json, 200, [], JSON_UNESCAPED_UNICODE);
+                    $toUser = User::find($request->to_user_id);
+                    if ($toUser->userPrivacy->no_follow_up_request == 0){
+                        $follow = new Follow();
+                        $follow->from_user_id = $user->id;
+                        $follow->to_user_id = $request->to_user_id;
+                        $follow->save();
+
+                        $json['status'] = 200;
+                        $json['message'] = "Success";
+                        return response()->json($json, 200, [], JSON_UNESCAPED_UNICODE);
+                    }else{
+                        $json['status'] = 204;
+                        $json['message'] = "Takip etmek istediğiniz kullanıcı takip isteği kabul etmiyor.";
+                        return response()->json($json, 200, [], JSON_UNESCAPED_UNICODE);
+                    }
                 } else {
                     $json['status'] = 0;
                     $json['message'] = "Takip edilecek kişi ID zorunludur.";
