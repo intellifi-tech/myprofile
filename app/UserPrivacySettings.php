8a152c326ccd883bc538855fafff5b547160abe8
Muharrem Özdemir <muharrem@clouditsoft.com>
2019-07-02 14:26:14 +0300

    MYP-20 geliştirme testleri

--- app/UserPrivacySettings.php
+++ app/UserPrivacySettings.php
@@ -7,4 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 class UserPrivacySettings extends Model
 {
     protected $table = "user_privacy_settings";
+
+    public function user()
+    {
+        return $this->hasOne('App\User', 'id', 'user_id');
+    }
 }
