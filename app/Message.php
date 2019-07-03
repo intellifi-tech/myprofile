82b0508eb2584eb32f794e91a191d0f68279bc7d
Muharrem <muharrem@clouditsoft.com>
2019-07-03 19:08:07 +0300

    Tüm mesajlarda son mesaj getirme

--- app/Message.php
+++ app/Message.php
@@ -7,4 +7,14 @@ use Illuminate\Database\Eloquent\Model;
 class Message extends Model
 {
     protected $table = "messages";
+
+    public function fromUser()
+    {
+        return $this->hasOne('App\User', 'id', 'from_user_id');
+    }
+
+    public function toUser()
+    {
+        return $this->hasOne('App\User', 'id', 'to_user_id');
+    }
 }
