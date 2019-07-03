83863f9a40004c4f2ec8085edf91fa268b2be50f
Muharrem <muharrem@clouditsoft.com>
2019-07-03 12:41:41 +0300

    Tüm firmaların çekilmesi

---
+++ app/Http/Controllers/Api/CompanyController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Company;
+use App\User;
+use Illuminate\Http\Request;
+
+class CompanyController extends Controller
+{
+    public function getCompanies(Request $request)
+    {
+        if ($request->header('api-token')) {
+            $user = User::where('api_token', $request->header('api-token'))->first();
+            if ($user) {
+                $companies = Company::all();
+                if ($companies->count() > 0){
+                    $json['status'] = 200;
+                    $json['message'] = "Success";
+                    $json['companies'] = $companies;
+                    return response()->json($json, 200, [], JSON_UNESCAPED_UNICODE);
+                }else{
+                    return response()->json(null, 404, [], JSON_UNESCAPED_UNICODE);
+                }
+            } else {
+                $json['status'] = 0;
+                $json['message'] = "Etkinlikler çekilemedi. Api_token geçersizdir.";
+                return response()->json($json, 200, [], JSON_UNESCAPED_UNICODE);
+            }
+        } else {
+            $json['status'] = 0;
+            $json['message'] = "api-token boş olamaz";
+            return response()->json($json, 200, [], JSON_UNESCAPED_UNICODE);
+        }
+    }
+}
