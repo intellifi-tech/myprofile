11397a046f5c37c1c098f0174e7ea23509e6791d
Muharrem <muharrem@clouditsoft.com>
2019-07-03 11:49:18 +0300

    Kullanıcının tüm mesajlarını getir apisine istek attığımda status 0 mesaj success geliyor. 0'ın anlamı nedir (edited)

--- app/Http/Controllers/Api/MessageController.php
+++ app/Http/Controllers/Api/MessageController.php
@@ -23,7 +23,7 @@ class MessageController extends Controller
                 }else{
                     $json['status'] = 204;
                     $json['message'] = "No Content";
-                    return response()->json($json, 200, [], JSON_UNESCAPED_UNICODE);
+                    return response()->json($json, 404, [], JSON_UNESCAPED_UNICODE);
                 }
             } else {
                 $json['status'] = 0;
