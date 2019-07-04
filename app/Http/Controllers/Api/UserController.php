aae1575b6f1923213463f3f49eeff4ea9343b6bf
Muharrem <muharrem@clouditsoft.com>
2019-07-04 19:00:42 +0300

    user işlemleri

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -67,6 +67,7 @@ class UserController extends Controller
 
             if (!is_null($request->company_title)){
                 $company = Company::where('name', $request->company_name)->first();
+                dd($company);
                 if ($company){
                     $user->company_id = $company->id;
                 }else{
