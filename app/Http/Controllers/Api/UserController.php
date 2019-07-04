4ee1e0ea34cdf13ae426b3bbb20d98533c671379
Muharrem <muharrem@clouditsoft.com>
2019-07-04 20:15:33 +0300

    user işlemleri

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -267,7 +267,7 @@ class UserController extends Controller
 
                 $json['status'] = 200;
                 $json['message'] = "Success";
-                $json['activities'] = $activities;
+                $json['activityy'] = $activities;
                 return response()->json($json, 200, [], JSON_UNESCAPED_UNICODE);
             } else {
                 $json['status'] = 204;
