9074f587933ae298d593fd3fe680678fb7751c16
Muharrem Özdemir <muharrem@clouditsoft.com>
2019-07-05 21:43:45 +0300

    sektör istatistiği

--- app/Http/Controllers/Admin/AjaxController.php
+++ app/Http/Controllers/Admin/AjaxController.php
@@ -87,22 +87,23 @@ class AjaxController extends Controller
             $nowMonthSectorUserCountsTmp[$sector->name] = $users;
         }
         arsort($nowMonthSectorUserCountsTmp);
+        return array_key_first($nowMonthSectorUserCountsTmp);
 
-        $sectorUserCounts[$nowMonth->format('m')] = array_key_first($nowMonthSectorUserCountsTmp) .' : '.$nowMonthSectorUserCountsTmp[array_key_first($nowMonthSectorUserCountsTmp)];
-
-        // Buraya kadar sorun yok.
-
-        $oneMonthSectorUserCountsTmp = [];
-        $sectors = Sector::all();
-        foreach ($sectors as $sector){
-            $users = User::where("type", 1)->where('sector_id', $sector->id)->whereMonth('created_at', $nowMonth->subMonth()->format('m'))->get()->count();
-            $oneMonthSectorUserCountsTmp[$sector->name] = $users;
-        }
-        arsort($oneMonthSectorUserCountsTmp);
-
-        $sectorUserCounts[$nowMonth->format('m')] = array_key_first($oneMonthSectorUserCountsTmp) .' : '.$oneMonthSectorUserCountsTmp[array_key_first($oneMonthSectorUserCountsTmp)];
-
-        return $sectorUserCounts;
+//        $sectorUserCounts[$nowMonth->format('m')] = array_key_first($nowMonthSectorUserCountsTmp) .' : '.$nowMonthSectorUserCountsTmp[array_key_first($nowMonthSectorUserCountsTmp)];
+//
+//        // Buraya kadar sorun yok.
+//
+//        $oneMonthSectorUserCountsTmp = [];
+//        $sectors = Sector::all();
+//        foreach ($sectors as $sector){
+//            $users = User::where("type", 1)->where('sector_id', $sector->id)->whereMonth('created_at', $nowMonth->subMonth()->format('m'))->get()->count();
+//            $oneMonthSectorUserCountsTmp[$sector->name] = $users;
+//        }
+//        arsort($oneMonthSectorUserCountsTmp);
+//
+//        $sectorUserCounts[$nowMonth->format('m')] = array_key_first($oneMonthSectorUserCountsTmp) .' : '.$oneMonthSectorUserCountsTmp[array_key_first($oneMonthSectorUserCountsTmp)];
+//
+//        return $sectorUserCounts;
     }
 
     public function eventStatistics(Request $request)
