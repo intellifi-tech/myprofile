53a056043645a3deb22aed3606a1cd699494c524
Muharrem Özdemir <muharrem@clouditsoft.com>
2019-07-05 22:03:26 +0300

    sektör istatistiği

--- app/Http/Controllers/Admin/AjaxController.php
+++ app/Http/Controllers/Admin/AjaxController.php
@@ -88,7 +88,7 @@ class AjaxController extends Controller
         }
         arsort($nowMonthSectorUserCountsTmp);
 
-        return array_key_first($nowMonthSectorUserCountsTmp);
+        return key($nowMonthSectorUserCountsTmp);
 //        $sectorUserCounts[$nowMonth->format('m')] = array_key_first($nowMonthSectorUserCountsTmp) .' : '.$nowMonthSectorUserCountsTmp[array_key_first($nowMonthSectorUserCountsTmp)];
 //
 //        // Buraya kadar sorun yok.
